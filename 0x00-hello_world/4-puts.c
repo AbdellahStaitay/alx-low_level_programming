5ec22cd4ff98eb18e73d608584b9fbb15389f5cf
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-16 15:54:54 +0100

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+/*
+ *  * main - Print a string
+ *   * Description: use puts
+ *    * Return: 0
+ *    
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
 
