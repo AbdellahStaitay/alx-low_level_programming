bed86038c36387513ae3cc7fbf0361d6a5fedd86
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-17 17:19:41 +0100

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
 * main - entry point
 * Description : prints the last digit and its nature based on condition
-* Return value : Allways 0 (succes)
+* Return value of main : Allways 0 (succes)
 */
 int main(void)
 {
