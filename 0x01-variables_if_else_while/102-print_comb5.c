28d5cfe6c65be798219636d614c30fbbd368872a
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-17 21:27:05 +0100

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,38 +6,37 @@
  */
 int main(void)
 {
-	int i, j, h, k;
-
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 57; j++)	
-		{
-            for (h = 48; h <= 57; h++)
-	        {
-		        for (k = 48; k <= 57; k++)
-                {
-                   if (((i + j) < (h + k) && (i <= h)) || i < h)
-                   {
-                        putchar(i);
-                        putchar(j);
-                        putchar(' ');
-                        putchar(h);
-                        putchar(k);
-                        if (i+j+h+k == 227 && i == 57 )
-                        {
-                            break;
-                        }
-                        else
-				        {
-					        putchar(44);
-					        putchar(32);
-				        }
-                   }
-                }
-            }
-        }    
-    }
-    putchar(10);
-	return (0);
+int i, j, h, k;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+for (h = 48; h <= 57; h++)
+{
+for (k = 48; k <= 57; k++)
+{
+if (((i + j) < (h + k) && (i <= h)) || i < h)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(h);
+putchar(k);
+if (i + j + h + k == 227 && i == 57)
+{
+break;
+}
+else
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
 }
 
