17ecb8a55719c21296c8b894a2444343b84ed01f
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-17 19:50:18 +0100

    3 digits combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,25 @@
  */
 int main(void)
 {
-	int i, j, h;
-
-	for (i = 48; i <= 57; i++)
-	{
-	for (j = 48; j <= 57; j++)
-    {
-    for (h = 48; h <= 57; h++)
-	if (i < j && j < h)
-	{
-	putchar(i);
-	putchar(j);
-    putchar(h);
-	if (h != 57 || j != 56 || i != 55)
-	{
-	putchar(44);
-	putchar(32);
-	}
-	}
-    }    
-	}
-	putchar(10);
-	return (0);
+int i, j, h;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+for (h = 48; h <= 57; h++)
+if (i < j && j < h)
+{
+putchar(i);
+putchar(j);
+putchar(h);
+if (h != 57 || j != 56 || i != 55)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
 }
