c458e7bfa00d61f741c741affb8b95a08e6a543c
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 14:26:30 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * alphabet - Entry point
+ * print_alphabet - Entry point
  * Description: prints string
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 	int i;
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
