e4ac25f475a218c7551dbda0fa0f2ed4db4fd71c
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 13:41:07 +0100

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {   
-    char text[] = "_putchar";
-    int i = 0;
-    while (i <= 7)
-    {
-        _putchar(text[i]);
-        i++;
-    }
-    _putchar('\n');
+	char text[] = "_putchar";
+	int i = 0;
+	while (i <= 7)
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
