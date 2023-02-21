45be1f16cdb554c5d9022c84adedbbd2da03bde0
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 14:45:14 +0100

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rint_alphabet_x10 - Entry point
+ * Description: prints string
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10 ; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,5 @@
 
 int _putchar(char ch);
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
 #endif
