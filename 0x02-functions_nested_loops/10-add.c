f347ba661b581976670834d7294d842dc5720c1f
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 16:36:57 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - check for lower case letters
+ * @n: input
+ * @c: input
+ *Return: 1 if c is lowercase
+ * 0 if c is anyhting else.
+ */
+int add(int n, int c)
+{
+	int r = n + c;
+
+	return (r);
+}
