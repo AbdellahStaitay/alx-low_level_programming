d52c82584cea620eaacf02cf881445683a956801
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 12:46:08 +0100

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,16 @@
  * Description: printd string
  * Return: Always 0 (success)
  */
+int prints(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
+/**
+ * print_alphabet - Entry point
+ * Description: printd alphabet
+ * Return: Always 0 (success)
+ */
 void print_alphabet(void)
 {
     int i;
