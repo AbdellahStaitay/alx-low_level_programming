77e5c7b9b6dadf5378f3a5ac32baa0b78a2a2a36
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-21 17:32:24 +0100

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *print_times_table - n times table.
+ *@n: the number defines the table.
+ *
+ *Return: no return value
+ */
+int main(void)
+{
+	int i;
+	int sum;
+	
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
