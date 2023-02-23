25c41bb4cd8c199c56a9633cd9e54c1da5a1c78f
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-23 17:18:36 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ * @c : input
+ * Return: Always 0 (success)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
