f8c2ce964f661d5dcd2274c802d88c7db6b7c320
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-23 18:28:19 +0100

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,16 @@ void print_square(int size)
 	int i;
 	int j;
 
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
-	}
-	if (size == 0)
-	{
 		_putchar('\n');
 	}
 }
