3e96d8f147eeaf3278218d592aa1a02547ae9ba2
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-27 18:00:41 +0100

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str : input
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i = 0;
+	int half;
+
+	while (str[len])
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		half = len / 2;
+		for (i = half; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half = (len - 1) / 2;
+		for (i = half; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
