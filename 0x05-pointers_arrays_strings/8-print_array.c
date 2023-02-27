a0f2e082d537b8178334092a09a3221b27c5959b
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-02-27 18:26:04 +0100

    updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,10 @@
 void print_array(int *a, int n)
 {
 	int i;
-	if (n >= 0)
+
+	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			if (i != n)
 			{
@@ -22,6 +23,13 @@ void print_array(int *a, int n)
 			{
 				printf("%d\n", a[i]);
 			}
+		}
+	}
+	else if (n == 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+				printf("%d\n", a[i]);
 		}
 	}
 }
