5db9501e2a220acf765163a4a624539bb1cb9355
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-01 11:21:15 +0100

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, len = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
