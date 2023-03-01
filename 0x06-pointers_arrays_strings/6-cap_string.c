37ebdafa514d7160011713532dce91b17b3259d4
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-01 16:02:28 +0100

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * *cap_string - upper string
+ * @text: input
+ * Return:pinter
+ */
+
+char *cap_string(char *text)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; text[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (text[i] == ',' || text[i] == ';' || text[i] == '.' ||
+		text[i] == '!' || text[i] == '?' || text[i] == '"' ||
+		text[i] == '(' || text[i] == ')' || text[i] == '{' ||
+		text[i] == '}' || text[i] == '\n' || text[i] == ' ' ||
+		text[i] == 9 || text[i] == 11)
+		{
+			if (text[i + 1] >= 97 && text[i + 1] <= 122)
+			{
+				text[i + 1] = text[i + 1] - 32;
+			}
+		}
+	}
+	return (text);
+}
