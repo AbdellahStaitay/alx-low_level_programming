cefa22f4809fd65d738c0f687d9fbb7f70f8f51d
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-10 18:46:34 +0100

    filename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file
+ * @argc: the counter of the command line arguments
+ * @argv: the arguemnet
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
