a35d54aa52daccd138eb11e00c3db8c2609ccace
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-13 11:33:26 +0100

    concatenate string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - create array
+ * @s1: input
+ * @s2: input
+ * Return: poniter or null
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int size1 = 0;
+	unsigned int size2 = 0;
+	unsigned int i;
+	unsigned int j = 0;
+
+	for (i = 0; s1[i]; i++)
+	{
+		size1++;
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		size2++;
+	}
+	p = malloc(sizeof(char) * (size1 + size2) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size1; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (i = size1; i < (sizeof(char) * (size1 + size2)); i++)
+		{
+			p[i] = s2[j];
+			j++;
+		}
+		p[i] = '\0';
+		return (p);
+	}
+}
