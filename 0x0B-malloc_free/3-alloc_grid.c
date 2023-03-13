36d96f7fb1fccbb5f36e447ecc6ff85a267bbb1b
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-13 12:43:13 +0100

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,21 +11,20 @@
 int **alloc_grid(int width, int height)
 {
 	int **p;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	p = calloc(height, sizeof(int *));
+	p = malloc(sizeof(int *) * height);
 	if (p == NULL)
 	{
-		free(p);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
-		p[i] = calloc(width, sizeof(int));
+		p[i] = malloc(sizeof(int) * width);
 
 		if (p[i] == NULL)
 		{
@@ -35,6 +34,11 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
 	}
 	return (p);
 }
