ffe74728b3863b9ea3a9256d00e1404ac2aee67d
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-13 12:36:19 +0100

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * alloc_grid - create array
+ * @width: input
+ * @height: input
+ * Return: poniter or null
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = calloc(height, sizeof(int *));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = calloc(width, sizeof(int));
+
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+
+			free(p);
+			return (NULL);
+		}
+	}
+	return (p);
+}
