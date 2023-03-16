62b18028810e5fcf6d5a9fbb9fe5767e54c68305
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-16 13:53:12 +0100

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			p[i] = s1[i];
 		}
-		for (j = 0; j < n ; j++)
+		for (j = 0; s2[j] && j < n ; j++)
 		{
 			p[i] = s2[j];
 			i++;
