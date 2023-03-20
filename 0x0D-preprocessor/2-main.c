8deac88c1ed97a63e99f7b66f7075fd772137b7d
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-20 08:17:45 +0000

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print the name of the file
+ * descriprtion : check the code
+ * Return: always 0
+*/
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (0);    
+	return (0);
 }
