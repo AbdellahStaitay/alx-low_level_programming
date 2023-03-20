2fd31d54138339d9644893fcaa72ab5d34bbb254
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-20 10:47:09 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - prints a struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to the structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (new_dog);
+	}
+}
