8d0fca4275f0731df407a423de5f3ed82113fd1c
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-22 19:32:19 +0000

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,22 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	else
+	if (size <= 0)
 	{
-		if (size <= 0)
-		{
-			return (0);
-		}
-		else
-		{
-			for (i = 0; i < size; i++)
-			{
-				if (cmp(array[i]) != 0)
-				{
-					return (i);
-				}
-			}
-		}
-		return (-1);
+		return (0);
 	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
