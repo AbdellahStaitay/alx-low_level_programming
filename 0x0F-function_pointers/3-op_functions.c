c31a7dbd12543edd6c2731584a475a66aa5f13d7
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-22 22:40:55 +0000

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - entry point
+ * @a: input
+ * @b: input
+ * Return: integer
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entry point
+ * @a: input
+ * @b: input
+ * Return: integer
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - entry point
+ * @a: input
+ * @b: input
+ * Return: integer
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - entry point
+ * @a: input
+ * @b: input
+ * Return: integer
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - entry point
+ * @a: input
+ * @b: input
+ * Return: integer
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
