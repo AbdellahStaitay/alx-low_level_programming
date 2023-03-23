97b90ad352a6a1c97aa44506c5ff3dbb5ae8a3b7
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-03-23 22:25:35 +0000

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,16 +6,15 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum;
+	unsigned int i;
+	va_list ptr;
 	if (n == 0)
 	{
 		return (0);
 	}
-	int sum = 0;
-	int i;
-	va_list ptr;
-
 	va_start(ptr, n);
-
+	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
