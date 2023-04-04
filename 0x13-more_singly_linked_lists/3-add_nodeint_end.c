d3614defb49662c56119799d2fd750b373487900
AbdellahStaitay <abdellahstaitay@gmail.com>
2023-04-04 17:14:02 +0000

    add nodes

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - add node to the list
+ * @head: linked lists
+ * @n: input
+ * Return: number of nodes
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *last, *ptr = malloc(sizeof(listint_t));
+
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	ptr->n = n;
+	ptr->next = NULL;
+	last = *head;
+
+	while (last != NULL)
+	{
+		last = last->next;
+	}
+	last->next = ptr;
+	return (*head);
+}
